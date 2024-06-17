6f304cc56858aa48510a7b1a7774634e3fc45821
to4to <justjustifying@outlook.com>
2024-06-18 01:12:36 +0530

    refactor: Update import statement for pytube library

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from pytube import Youtube
+from pytube import YouTube
 from youtube_transcript_api import YouTubeTranscriptApi
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_openai import ChatOpenAI
@@ -17,6 +17,13 @@ load_dotenv(find_dotenv())
 #load Api Key
 
 youtube_url="https://www.youtube.com/watch?v=2TJxpyO3ei4"
+
+
+
+#download the video
+
+os.makedirs("downloaded_video",exist_ok=True)
+
 
 
 
